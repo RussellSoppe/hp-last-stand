7590c1fd2e2d89b29c4c9a2eda84de2d73d70b60
russellicious <42648750+russellicious@users.noreply.github.com>
2019-02-19 10:38:45 -0600

    Updates

---
+++ precache-manifest.59b7448449941a521bb937960ca83877.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "94cc1e4dae0ed5c7d82372c1614d84a0",
+    "url": "/hp-last-stand/static/media/Hufflepuff_Crest.94cc1e4d.png"
+  },
+  {
+    "revision": "895a478af188251cb7b4",
+    "url": "/hp-last-stand/static/css/main.5bb246a4.chunk.css"
+  },
+  {
+    "revision": "f181d4e8b62c2a91b6d3",
+    "url": "/hp-last-stand/static/js/runtime~main.f181d4e8.js"
+  },
+  {
+    "revision": "9931cdce83b566ddcf5c",
+    "url": "/hp-last-stand/static/js/2.9931cdce.chunk.js"
+  },
+  {
+    "revision": "7b120b3cbe66c93e50ff78388d3f227e",
+    "url": "/hp-last-stand/static/media/Gryffindorcrest.7b120b3c.png"
+  },
+  {
+    "revision": "6911b896fb4d711f4d6784384467c24b",
+    "url": "/hp-last-stand/static/media/Slytherin_Crest.6911b896.png"
+  },
+  {
+    "revision": "895a478af188251cb7b4",
+    "url": "/hp-last-stand/static/js/main.895a478a.chunk.js"
+  },
+  {
+    "revision": "9da392cf1bc9de55de181a5c070f4d0f",
+    "url": "/hp-last-stand/static/media/Ravenclaw_Crest.9da392cf.png"
+  },
+  {
+    "revision": "0b9c30c1c9e2ca0e9dad79a4eba90b73",
+    "url": "/hp-last-stand/static/media/hponbroom.0b9c30c1.png"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/hp-last-stand/static/media/HARRYP__.181ef9a7.TTF"
+  },
+  {
+    "revision": "3e97dbdbf4aa4665df1da7bc816d93ac",
+    "url": "/hp-last-stand/static/media/Oswald-Medium.3e97dbdb.ttf"
+  },
+  {
+    "revision": "c2cd2c1296cfec944fd84244aafb6878",
+    "url": "/hp-last-stand/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hp-last-stand/precache-manifest.59b7448449941a521bb937960ca83877.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hp-last-stand/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
