7e2cf4a18d74863b536817c027adcbd827f2a6e5
Russell S <russsoppe@gmail.com>
2021-02-07 17:03:14 -0600

    added ui feedback to avatar choices

--- src/components/AvatarChoices/AvatarChoices.js
+++ src/components/AvatarChoices/AvatarChoices.js
@@ -11,36 +11,44 @@ const AvatarChoices = ({onAvatarChange1, onAvatarChange2, avatar1choice, avatar2
 
 
 	return(
+
 		<div id="avatarchoicesmaindiv">
-			<div id="avatarchoicetitle">Avatar Choices</div>
-				<div className="avatarchoicediv">
-					<img src={hufflepuffavatar} alt="Hufflepuff Avatar" height="75px" length="75px"></img>
-					<div className="avatarchoicebuttonsdiv">
-						<div className="avatarchoicebutton"><button disabled = {avatar1choice===true || avatar2==="Hufflepuff"} onClick={()=>onAvatarChange1(hufflepuffavatar, "Hufflepuff")}>Player 1</button></div>
-						<div className="avatarchoicebutton"><button disabled = {avatar2choice===true || avatar1==="Hufflepuff"} onClick={()=>onAvatarChange2(hufflepuffavatar, "Hufflepuff")}>Player 2</button></div>
-					</div>
+
+			<div id="avatarchoicetitle"> Avatar Choices </div>
+
+			<div className="avatarchoicediv">
+
+				<img src={hufflepuffavatar} alt="Hufflepuff Avatar" height="75px" length="75px"/>
+				<div className="avatarchoicebuttonsdiv">
+					<div><button className={avatar1choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar1choice===true || avatar2==="Hufflepuff"} onClick={()=>onAvatarChange1(hufflepuffavatar, "Hufflepuff")}>Player 1</button></div>
+					<div><button className={avatar2choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar2choice===true || avatar1==="Hufflepuff"} onClick={()=>onAvatarChange2(hufflepuffavatar, "Hufflepuff")}>Player 2</button></div>
 				</div>
-				<div className="avatarchoicediv">
-					<img src={griffindoravatar} alt="Gryffindor Avatar" height="75px" length="75px"></img>
-					<div className="avatarchoicebuttonsdiv">
-						<div className="avatarchoicebutton"><button disabled = {avatar1choice===true || avatar2==="Gryffindor"} onClick={()=>onAvatarChange1(griffindoravatar, "Gryffindor")}>Player 1</button></div>
-						<div className="avatarchoicebutton"><button disabled = {avatar2choice===true || avatar1==="Gryffindor"} onClick={()=>onAvatarChange2(griffindoravatar, "Gryffindor")}>Player 2</button></div>
-					</div>
+			</div>
+
+			<div className="avatarchoicediv">
+				<img src={griffindoravatar} alt="Gryffindor Avatar" height="75px" length="75px" />
+				<div className="avatarchoicebuttonsdiv">
+					<div ><button className={avatar1choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar1choice===true || avatar2==="Gryffindor"} onClick={()=>onAvatarChange1(griffindoravatar, "Gryffindor")}>Player 1</button></div>
+					<div ><button className={avatar2choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar2choice===true || avatar1==="Gryffindor"} onClick={()=>onAvatarChange2(griffindoravatar, "Gryffindor")}>Player 2</button></div>
 				</div>
-				<div className="avatarchoicediv">
-					<img src={ravenclawavatar} alt="Ravenclaw Avatar" height="75px" length="75px"></img>
-					<div className="avatarchoicebuttonsdiv">
-						<div className="avatarchoicebutton"><button disabled = {avatar1choice===true || avatar2==="Ravenclaw"} onClick={()=>onAvatarChange1(ravenclawavatar, "Ravenclaw")}>Player 1</button></div>
-						<div className="avatarchoicebutton"><button disabled = {avatar2choice===true || avatar1==="Ravenclaw"} onClick={()=>onAvatarChange2(ravenclawavatar, "Ravenclaw")}>Player 2</button></div>
-					</div>
+			</div>
+
+			<div className="avatarchoicediv">
+				<img src={ravenclawavatar} alt="Ravenclaw Avatar" height="75px" length="75px"/>
+				<div className="avatarchoicebuttonsdiv">
+					<div><button className={avatar1choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar1choice===true || avatar2==="Ravenclaw"} onClick={()=>onAvatarChange1(ravenclawavatar, "Ravenclaw")}>Player 1</button></div>
+					<div><button className={avatar2choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar2choice===true || avatar1==="Ravenclaw"} onClick={()=>onAvatarChange2(ravenclawavatar, "Ravenclaw")}>Player 2</button></div>
 				</div>
-				<div className="avatarchoicediv">
-					<img src={slytherinavatar} alt="Slytherin Avatar" height="75px" length="75px"></img>
-					<div className="avatarchoicebuttonsdiv">
-						<div className="avatarchoicebutton"><button disabled = {avatar1choice===true || avatar2==="Slytherin"} onClick={()=>onAvatarChange1(slytherinavatar, "Slytherin")}>Player 1</button></div>
-						<div className="avatarchoicebutton"><button disabled = {avatar2choice===true || avatar1==="Slytherin"} onClick={()=>onAvatarChange2(slytherinavatar, "Slytherin")}>Player 2</button></div>
-					</div>
+			</div>
+
+			<div className="avatarchoicediv">
+				<img src={slytherinavatar} alt="Slytherin Avatar" height="75px" length="75px"/>
+				<div className="avatarchoicebuttonsdiv">
+					<div><button className={avatar1choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar1choice===true || avatar2==="Slytherin"} onClick={()=>onAvatarChange1(slytherinavatar, "Slytherin")}>Player 1</button></div>
+					<div><button className={avatar2choice===!true ? "avatarchoicebutton" : "disabled"} disabled = {avatar2choice===true || avatar1==="Slytherin"} onClick={()=>onAvatarChange2(slytherinavatar, "Slytherin")}>Player 2</button></div>
 				</div>
+			</div>
+
 		</div>
 		);
 }
